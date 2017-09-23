03e83a48b979e558491789ab415327efc053b7f6
Abraao Levi <abr4ao.levi@gmail.com>
2017-09-23 12:08:47 -0300

    adding movie model and movie service

--- AppRouletteNetFlix/Model/Movie.cs
+++ AppRouletteNetFlix/Model/Movie.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace AppRouletteNetFlix.Model
 {
     public class Movie
     {
-        public Movie()
-        {
-        }
+        [JsonProperty("show_title")]
+        public string Title { get; set; }
+
+        [JsonProperty("release_year")]
+        public int ReleaseYear { get; set; }
+
+        [JsonProperty("poster")]
+        public string ImageUrl { get; set; }
+
+        [JsonProperty("summary")]
+        public string Summary { get; set; }
     }
 }
--- AppRouletteNetFlix/Service/MovieService.cs
+++ AppRouletteNetFlix/Service/MovieService.cs
@@ -1,10 +1,37 @@
 ﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using AppRouletteNetFlix.Model;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace AppRouletteNetFlix.Service
 {
     public class MovieService
     {
-        public MovieService()
+        private HttpClient _client = new HttpClient();
+        private List<Movie> _movies;
+
+        public async Task<List<Movie>> LocalizarFilmesPorAtor(string ator)
         {
+            if (string.IsNullOrWhiteSpace(ator)) {
+                return null;
+            }
+
+            string url = string.Format("http://netflixroulette.net/api/api.php?actor={0}", ator);
+            var response = await _client.GetAsync(url);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.NotFound) {
+                _movies = new List<Movie>();
+                return _movies;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+
+            _movies = new List<Movie>(movies);
+            return _movies;
         }
+
     }
 }
